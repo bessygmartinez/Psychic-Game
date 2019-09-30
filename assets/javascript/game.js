cc9158088b6ef771bb637e1ffe3344d674e5a39e
Bessy Gee <nakiwarai13@gmail.com>
2019-09-30 16:20:28 -0400

    added else statement and corrected player's guess if statement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,35 @@
 //Computer chooses from array below
 let randomLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+//Stats to zero
+let wins = 0;
+let losses = 0;
+let guessesLeft = 10;
+let guessesMade = [];
+let userGuess = null;
+
+// Function to restart the stats
+let restart = function(){
+    guessesLeft = 10;
+    guessesMade = [];
+    compChoice = randomLetter[Math.floor(Math.random() * randomLetter.length)];
+}
+
 //This will get the program to choose a random letter from the array above
 let compChoice = randomLetter[Math.floor(Math.random() * randomLetter.length)];
 
 console.log(compChoice);
 
-//Stats to zero
-let wins = 0;
-let losses = 0;
-let guessesLeft = 9;
-let guessesMade = [];
-let userGuess = null;
-
-let restart = function(){
-    guessesLeft = 9;
-    guessesMade = [];
-    compChoice = randomLetter[Math.floor(Math.random() * randomLetter.length)];
-}
-
-
 //function for key presses
 document.onkeyup = function () {
 
-    //Player presses key, key stroke is stored to this variable
-    var playerKey = String.fromCharCode(event.keyCode).toLowerCase();
-    console.log("The user guessed " + playerKey);
+//This is the player's choice and is stored in the variable
+let userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+console.log("The player guessed " + userGuess);
+
 
     //Player's guess is compared to computer's choice
-    if (playerKey == compChoice) {
+    if (userGuess == compChoice) {
         console.log("same");
 
         //win count
@@ -36,10 +37,27 @@ document.onkeyup = function () {
         console.log("The user has won " + wins + " times.");
 
         //reset numGuesses count to 9
-        guessesLeft = 9;
+        guessesLeft = 10;
 
         restart();
 
         console.log(compChoice);
+    }
+
+    //Player's guess is compared to computer's choice and is not a match
+    else {
+        console.log("different");
+        guessesMade++;
+        guessesLeft--;
+        console.log(guessesMade);
+        console.log(guessesLeft);
+        document.querySelector("#playerGuesses").innerHTML += userGuess + " ";
+        console.log("the user has guessed a total of " + guessesMade + " times.");
+            if (guessesLeft == 0){
+                losses++;
+                guessesLeft=10;
+                userGuess = null;
+                document.querySelector("#playerGuesses").innerHTML = userGuess;
+            }
     }
 }
