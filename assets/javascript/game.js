2085837d823d947957df6a009da06e840f60b82f
Bessy Gee <nakiwarai13@gmail.com>
2019-09-30 14:11:23 -0400

    added function for key presses, keystroke storage to variable, player guess comparison to computer's choice, number wins count up and number of guesses reset to 9

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,9 +4,31 @@ let randomLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
 //This will get the program to choose a random letter from the array above
 let compChoice = randomLetter[Math.floor(Math.random() * randomLetter.length)];
 
+console.log(compChoice);
 
 //Stats to zero
 let numWins = 0;
 let numLosses = 0;
 let numGuesses = 9;
 let playerGuesses = [];
+let userGuess = null;
+
+//function for key presses
+document.onkeyup = function () {
+
+    //Player presses key, key stroke is stored to this variable
+    var playerGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    console.log("The user guessed " + userGuess);
+
+    //Player's guess is compared to computer's choice
+    if (playerGuess == compChoice) {
+        console.log("same");
+
+        //win count
+        numWins++;
+        console.log("The user has won " + numWins + " times.");
+
+        //reset numGuesses count to 9
+        numGuesses = 9;
+    }
+}
