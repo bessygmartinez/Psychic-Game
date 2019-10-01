11ca897eb35e6612f9777b9c9f555ca224cdab6b
Bessy Gee <nakiwarai13@gmail.com>
2019-10-01 11:30:31 -0400

    corrected guessesLeft to 10 in if statement for guesses left. Corrected commenting for more accuracy of descriptions.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-//Computer chooses from array below
+//Array of letters to choose from
 let randomLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 //Stats to zero
@@ -8,13 +8,13 @@ let guessesLeft = 10;
 let guessesMade = [];
 let userGuess = null;
 
-//This will get the program to choose a random letter from the array above
+//Program will choose random letter from array above and stored in variable compChoice
 let compChoice = randomLetter[Math.floor(Math.random() * randomLetter.length)];
 
 console.log(compChoice);
 
 // Function to restart the stats
-let restart = function(){
+let restart = function () {
     guessesLeft = 10;
     guessesMade = [];
     userGuess = null;
@@ -25,43 +25,57 @@ let restart = function(){
 //function for key presses
 document.onkeyup = function () {
 
-//This is the player's choice and is stored in the variable
-let userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-console.log("The player guessed " + userGuess);
+    //Player presses a key and the input is converted to lowercase and stored in the variable userGuess
+    let userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+    //Program console logs the player's guess
+    console.log("The player guessed " + userGuess);
 
 
-    //Player's guess is compared to computer's choice
+    //userGuess is compared to compChoice to check if same
     if (userGuess == compChoice) {
-        console.log("same");
+        console.log("Same letter chosen.");
 
-        //win count
+        //win count goes up by one
         wins++;
         console.log("The user has won " + wins + " times.");
 
+        //Call function to restart the stats
         restart();
 
+        //Program console logs the next random letter
         console.log(compChoice);
     }
 
-    //Player's guess is compared to computer's choice and is not a match
+    //If not the same letter, program will console log as such
     else {
-        console.log("different");
+        console.log("Not the same letter chosen by the computer");
+
+        //guessesMade goes up by 1, guessesLeft goes down by 1
         guessesMade++;
         guessesLeft--;
-        console.log(guessesMade);
-        console.log(guessesLeft);
 
-        //Player's key choice is displayed
+        //Program console logs how many guesses have been made and how many are left
+        console.log("Player has guessed " + guessesMade + " times and has " + guessesLeft + " guesses left");
+
+        //Program prints in playerGuesses span which keys have been pressed
         document.querySelector("#playerGuesses").innerHTML += userGuess + " ";
-        console.log("the user has guessed a total of " + guessesMade + " times.");
-            if (guessesLeft == 0){
-                losses++;
-                guessesLeft=9;
-                userGuess = null;
-                document.querySelector("#playerGuesses").innerHTML = userGuess;
-            }
+
+    //If player runs out of guesses (guessesLeft is at 0)
+    if (guessesLeft == 0) {
+
+            //Losses goes up by one
+            losses++;
+
+            //Program restarts the stats
+            restart();
+
+            //Program console logs the next random letter
+            console.log(compChoice);
+        }
     }
 
+    //This will print the wins, losses, and guesses left to spans with corresponding IDs
     document.querySelector("#numWins").innerHTML = wins;
     document.querySelector("#numLosses").innerHTML = losses;
     document.querySelector("#guessesLeft").innerHTML = guessesLeft;
